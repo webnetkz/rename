1eecad7100974de8b7bfeaa09b24100df2d349d1
webnetkz <dbqqbq@gmail.com>
2023-08-23 00:00:11 +0600

    Update beta

--- popup.js
+++ popup.js
@@ -1,6 +1,25 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.color) {
-    var colorDisplay = document.getElementById('colorDisplay');
-    colorDisplay.textContent = 'Цвет под курсором: #' + message.color;
-  }
+document.addEventListener("DOMContentLoaded", () => {
+    // Ожидаем 4 секунды (4000 миллисекунд) и затем закрываем всплывающее окно
+    setTimeout(() => {
+      window.close();
+    }, 10);
+
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const activeTab = tabs[0];
+        if (activeTab) {
+            // Очистка хранилищ
+            chrome.storage.local.clear();
+            chrome.storage.session.clear();
+
+            // Очистка cookie
+            chrome.cookies.getAll({ url: activeTab.url }, (cookies) => {
+            for (const cookie of cookies) {
+                chrome.cookies.remove({ url: activeTab.url, name: cookie.name });
+            }
+
+            // Перезагрузка вкладки
+            chrome.tabs.reload(activeTab.id, { bypassCache: true });
+            });
+        }
+    });
 });
