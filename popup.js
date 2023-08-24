bf9c2b30da2de64ec173bf6e018f02d3ee779650
webnetkz <dbqqbq@gmail.com>
2023-08-24 23:53:06 +0600

    Refactoring all code base

--- popup.js
+++ popup.js
@@ -1,25 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Ожидаем 4 секунды (4000 миллисекунд) и затем закрываем всплывающее окно
-    setTimeout(() => {
-      window.close();
-    }, 10);
-
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     const activeTab = tabs[0];
-        if (activeTab) {
-            // Очистка хранилищ
-            chrome.storage.local.clear();
-            chrome.storage.session.clear();
-
-            // Очистка cookie
-            chrome.cookies.getAll({ url: activeTab.url }, (cookies) => {
-            for (const cookie of cookies) {
-                chrome.cookies.remove({ url: activeTab.url, name: cookie.name });
-            }
-
-            // Перезагрузка вкладки
-            chrome.tabs.reload(activeTab.id, { bypassCache: true });
-            });
-        }
-    });
+    if (activeTab) {
+      chrome.scripting.executeScript({
+        target: {tabId: activeTab.id},
+        files: ["content.js"]
+      });
+    }
 });
